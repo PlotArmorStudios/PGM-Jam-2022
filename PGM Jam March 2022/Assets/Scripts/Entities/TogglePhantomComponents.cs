daf9067845380f44ed00f6b6115be1de14d16dfe
Protagonist <49970830+PlotArmorStudios@users.noreply.github.com>
2022-03-26 17:11:55 -0400

    Phantom
    Phantom is immobile when lights are on
    Mobile with lights are off

---
+++ PGM Jam March 2022/Assets/Scripts/Entities/TogglePhantomComponents.cs
@@ -0,0 +1,14 @@
+public class TogglePhantomComponents : ToggleComponents
+{
+    private void OnEnable()
+    {
+        GameManager.OnTurnOnLanterns += ToggleOffComponents;
+        GameManager.OnTurnOffLanterns += ToggleOnComponents;
+    }
+    
+    private void OnDisable()
+    {
+        GameManager.OnTurnOnLanterns -= ToggleOffComponents;
+        GameManager.OnTurnOffLanterns -= ToggleOnComponents;
+    }
+}
