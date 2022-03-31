e90395b58cf3dc3bac375fad10ef8148bc8bdbfd
marksamanicomposer <marksamanicomposer@gmail.com>
2022-03-31 19:04:25 -0400

    FMOD Integration + Music Test
    - Added FMOD/Unity Integration package
    - Replaced Audio Listener on Main Camera with FMODStudioListener
    - Disabled Unity Audio
    - Added FMOD project file to FMOD Project folder in Assets folder
    - Set FMOD project path to the project's location in repo
    - Added FMOD folder to Scripts folder
    
    - Created F_MusicObject and F_MusicPlayer.cs to test with music file

---
+++ PGM Jam March 2022/Assets/Scripts/FMOD/F_MusicPlayer.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+public class F_MusicPlayer : MonoBehaviour
+{
+    [SerializeField]
+    [EventRef]
+    private string _gameplayMusic;
+
+    private EventInstance _musicInst;
+
+    void Start()
+    {
+        _musicInst = RuntimeManager.CreateInstance(_gameplayMusic);
+        _musicInst.start();
+        _musicInst.release();
+    }
+
+    void OnDestroy()
+    {
+        _musicInst.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+    }
+}
