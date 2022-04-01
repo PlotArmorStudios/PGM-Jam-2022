137cb3a0bac7fe85f417ac0f293f7df120f6dfc7
Protagonist <49970830+PlotArmorStudios@users.noreply.github.com>
2022-04-01 16:58:44 -0400

    Implemented purify event

--- PGM Jam March 2022/Assets/Scripts/Game Objects/MasterLantern.cs
+++ PGM Jam March 2022/Assets/Scripts/Game Objects/MasterLantern.cs
@@ -1,4 +1,39 @@
+using System;
+using UnityEngine;
+
 public class MasterLantern : Lantern
 {
-    
+    private ParticleSystem _particleSystem;
+    private bool _playerInRange;
+
+    private void Start()
+    {
+        _particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && _playerInRange)
+        {
+            Purify();
+        }
+    }
+
+    private void Purify()
+    {
+        _particleSystem.Play();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var player = other.GetComponent<Player>();
+        if (!player) return;
+        _playerInRange = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        var player = other.GetComponent<Player>();
+        if (!player) return;
+        _playerInRange = false;
+    }
 }
