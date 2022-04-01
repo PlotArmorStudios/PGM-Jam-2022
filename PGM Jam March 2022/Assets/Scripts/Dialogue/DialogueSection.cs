ff6288870e4b25e30041db5948d3ed8960bf09ee
hashbert <obert.chen@gmail.com>
2022-03-31 21:30:15 -0400

    attempted to deactivate player and disable cursor lock in DialogueSection script

--- PGM Jam March 2022/Assets/Scripts/Dialogue/DialogueSection.cs
+++ PGM Jam March 2022/Assets/Scripts/Dialogue/DialogueSection.cs
@@ -24,6 +24,9 @@ public class DialogueSection : MonoBehaviour
         numOfSentences = sentences.Length;
         continueButton.onClick.AddListener(ContinueButtonPressed);
         DisplayNextSen();
+        GameManager.Instance.DeactivatePlayer();
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
     private int currentSentenceIndex = 0;
@@ -32,6 +35,7 @@ public class DialogueSection : MonoBehaviour
         if (currentSentenceIndex >= sentences.Length)
         {
             dialogueSection.Play("DialogueFlyOut", -1, 0f);
+            GameManager.Instance.ActivatePlayer();
             return;
         }
         dialogue.Play("TextFadeIn", -1, 0f);
