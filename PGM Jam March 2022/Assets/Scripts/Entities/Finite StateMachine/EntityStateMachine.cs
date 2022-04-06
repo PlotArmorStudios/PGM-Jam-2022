2a91dc21453a9e9fbe917cd71861ba26cfa42897
Protagonist <49970830+PlotArmorStudios@users.noreply.github.com>
2022-04-06 11:45:11 -0400

    Fixed build errors

--- PGM Jam March 2022/Assets/Scripts/Entities/Finite StateMachine/EntityStateMachine.cs
+++ PGM Jam March 2022/Assets/Scripts/Entities/Finite StateMachine/EntityStateMachine.cs
@@ -2,7 +2,6 @@
 
 using System;
 using System.Collections;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Serialization;
--- PGM Jam March 2022/Assets/Scripts/Game Objects/SoulShard.cs
+++ PGM Jam March 2022/Assets/Scripts/Game Objects/SoulShard.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.Build.Content;
 using UnityEngine;
 using FMODUnity;
 
